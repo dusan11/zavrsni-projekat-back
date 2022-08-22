59793af03f22aa5636bb076d7e2e140b26dfc837
Dusan Matkovic <matkovic.dusan99@gmail.com>
2022-08-22 15:05:42 +0200

    Created Controller and Routes for Order

---
+++ laravel/app/Http/Controllers/OrderController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function index() 
+    {
+        $data = Order::with('orderType', 'orderStatus', 'company', 'products')->orderBy('id', 'DESC')->get();
+        return $data;
+    }
+
+    public function show($id)
+    {
+        $data = Order::with('orderType', 'orderStatus', 'company', 'products')->findOrFail($id);
+        return $data;
+    }
+
+    public function store(Request $request)
+    {
+        $order = Order::insert([
+            'date' => $request->date,
+            'order_type_id' => $request->orderType,
+            'company_id' => $request->company,
+            'order_status_id' => $request->orderStatus,
+            'total_price' => $request->totalPrice,
+            'created_at' =>  date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        
+        $respData['status']=201;
+        $respData['message']='Uspješno kreirano.';
+        $respData['order']=$order;
+
+        return response()->json($respData);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $order = Order::find($id);
+      
+        $order->update([
+            'date' => $request->date,
+            'order_type_id' => $request->orderType,
+            'company_id' => $request->companyId,
+            'order_status_id' => $request->orderStatusId,
+            'total_price' => $request->total_price,
+            'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+
+        $respData['status']=204;
+        $respData['message']='Uspješno ažurirano.';
+        $respData['data']=$order;
+
+        return response()->json($respData);
+    }
+
+    public function updateTotalPrice(Request $request, $id)
+    {
+        $order = Order::find($id);
+      
+        $order->update([
+            'total_price' => $request->total_price,
+            'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+
+        $respData['status']=204;
+        $respData['message']='Ukupna cijena uspješno ažurirana.';
+        $respData['data']=$order;
+
+        return response()->json($respData);
+    }
+
+    public function destroy($id)
+    {
+        $order = Order::findOrFail($id);
+        $order->delete($id);
+       
+        $respData['status']=204;
+        $respData['message']='Uspješno obrisano.';
+       
+        return response()->json($respData);
+    }
+
+    public function removeProduct($order_id, $product_id)
+    {
+        $order = Order::find($order_id);
+        $order->products()->detach($product_id);
+        
+        $respData['status']=200;
+        $respData['message']='Proizvod uspješno uklonjen.';
+        
+        return response()->json($respData);
+    }
+
+    public function addProduct(Request $request, $order_id, $product_id)
+    {
+        $order = Order::find($order_id);
+        $order->products()->attach($product_id, ['amount' => $request->input('amount')]);
+        
+        $respData['status']=200;
+        $respData['message']='Proizvod uspješno dodat.';
+        
+        return response()->json($respData);
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\OrderTypeController;
 use App\Http\Controllers\OrderStatusController;
 
+use App\Http\Controllers\OrderController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,3 +29,12 @@ Route::resource('proizvodi', ProductController::class);
 Route::resource('kompanija', CompanyController::class);
 Route::resource('vrsta-narudzbe', OrderTypeController::class);
 Route::resource('status-narudzbe', OrderStatusController::class);
+
+Route::get('narudzbe', [OrderController::class, 'index']);
+Route::get('narudzbe/{id}', [OrderController::class, 'show']);
+Route::post('narudzbe', [OrderController::class, 'store']);
+Route::put('narudzbe/{id}', [OrderController::class, 'update']);
+Route::put('narudzbe/{id}/cijena', [OrderController::class, 'updateTotalPrice']);
+Route::delete('narudzbe/{id}', [OrderController::class, 'destroy']);
+Route::post('narudzbe/{narudzba_id}/proizvodi/{proizvod_id}', [OrderController::class, 'addProduct']);
+Route::delete('narudzbe/{narudzba_id}/proizvodi/{proizvod_id}', [OrderController::class, 'removeProduct']);
